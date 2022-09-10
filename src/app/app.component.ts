5968912534d21110e34e5515167fb695481462b7
kevin-mehta <kevin.h.mehta@gmail.com>
2022-09-10 21:41:08 +0530

    Imported required JS libraries.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, VERSION } from '@angular/core';
+import './lib/jquery/jquery-1.10.2.min.js';
+import './lib/jquery/jquery-ui.js';
+import './vendor/edq/myAccountEdqScript.js';
+// import './lib/jquery/jquery-ui.css';
 
 @Component({
   selector: 'my-app',
